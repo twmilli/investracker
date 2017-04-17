a53732cb594d05d2658ba5810ed12c0a5d293768
Taylor Milliman <twm013@bucknell.edu>
2017-04-17 14:35:41 -0400

    testing

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('babel-register');
 const app = require('./server/app').app;
+const express = require('express');
 
 app.set('port', (process.env.PORT || 3001));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static('client/build'));
+}
 
 app.listen(app.get('port'), () => {
   console.log('listening on port ' + app.get('port'));
