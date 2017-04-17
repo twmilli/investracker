193f330d01fc53575f7c1d74554f9e13c402ec34
Taylor Milliman <twm013@bucknell.edu>
2017-04-17 14:40:18 -0400

    fixing bug

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const express = require('express');
 
 app.set('port', (process.env.PORT || 3001));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static('client/build'));
-}
+app.use('/', express.static('./client/build'));
+
 
 app.listen(app.get('port'), () => {
   console.log('listening on port ' + app.get('port'));
